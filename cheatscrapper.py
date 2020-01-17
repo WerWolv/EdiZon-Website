7dd07fec91fbdb0e85627da8fbcbe83952b1b3d1
jr638091 <jr638091@gmail.com>
2020-01-17 16:46:09 -0500

    cached and optimizing scraper

--- cheatscrapper.py
+++ cheatscrapper.py
@@ -2,8 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 from flask import Flask
 import json
+import datetime
 
 app = Flask(__name__)
+
+debug = True
+
+cache_top = None
+cache_recent = None
+
+def log(txt):
+    if debug:
+        print(txt)
 
 def process(link):
     html = requests.get('https://www.switchcheatsdb.com' + link).content.decode()
@@ -16,27 +26,48 @@ def process(link):
 
 @app.route('/tops')
 def get_top_cheats():
-    html = requests.get('https://www.switchcheatsdb.com/hof').content.decode()
-    soap = BeautifulSoup(html, 'html.parser')
-    divs = soap.find_all('div',"container")
-    tops = divs[1]
-    tops = tops.contents[7].find_all('a')
-    links = [{'rank': i.next_sibling.next_sibling.div.string, 'link':i.get('href')} for i in tops]
-    for i in links:
-        i.update(process(i['link']))
-    return json.dumps(links)
+    global cache_top
+    time = datetime.datetime.now()
+    if cache_top is None or (cache_top['time'] - time).total_seconds() > 60*24:
+        html = requests.get('https://www.switchcheatsdb.com/hof').content.decode()
+        soap = BeautifulSoup(html, 'html.parser')
+        divs = soap.find_all('div',"container")
+        tops = divs[1]
+        tops = tops.contents[7].find_all('a')
+        links = [{'rank': i.next_sibling.next_sibling.div.string, 'link':i.get('href')} for i in tops]
+        for i in links:
+            print(f'top\t ===> \t {i}')
+            i.update(process(i['link']))
+        cache_top = {'time':time, 'data':links}
+        return json.dumps(links)
+    return json.dumps(cache_top['data'])
 
 @app.route('/recents')
 def get_recent_cheats():
-    html = requests.get('https://www.switchcheatsdb.com/').content.decode()
-    soap = BeautifulSoup(html, 'html.parser')
-    divs = soap.find_all('div',"container")
-    tops = divs[1]
-    tops = tops.contents[7].find_all('a')
-    links = [{'link': x.get('href')} for x in tops]
-    for i in links:
-        i.update(process(i['link']))
-    return json.dumps(links)
+    global cache_recent
+    time = datetime.datetime.now()
+    if cache_recent is None or (cache_recent['time'] - time).total_seconds() > 60*24:
+        html = requests.get('https://www.switchcheatsdb.com/').content.decode()
+        soap = BeautifulSoup(html, 'html.parser')
+        divs = soap.find_all('div',"container")
+        tops = divs[1]
+        tops = tops.contents[7].find_all('a')
+        links = [{'link': x.get('href'), 'img': x.img.get('src'), 'tittle': x.next_sibling.next_sibling.div.string} for x in tops]
+        # for i in links:
+        #     print(f'recent\t ===> \t {i}')
+        #     i.update(process(i['link']))
+        cache_recent = {'time':time, 'data':links}
+        return json.dumps(links)
+    return json.dumps(cache_recent['data'])
+
+@app.route('/')
+def home():
+    return 'api on progress:<br>\
+        <a href="\\recents"> recents cheats </a><br>\
+        <a href="\\tops"> top cheats </a>\
+        '
+
+
 
 if __name__ == '__main__':
     app.env = 'development'
